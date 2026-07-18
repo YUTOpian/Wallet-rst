5fa37fe93cc63ded899ba1d3c3aa900ab7033274
YUTO <63623900+YUTOpian@users.noreply.github.com>
2026-07-18 12:38:44 +0900

    Update transfer.js

--- js/transfer.js
+++ js/transfer.js
@@ -72,12 +72,27 @@ const amount = Number(amountStr);
   メッセージ
 */
 
-const payload =
-  await createMessagePayload(
+let payload;
+
+
+if (encryptMessage) {
+
+  await window.SSS.setEncryptedMessage(
     messageText,
-    encryptMessage,
     recipientPublicKey
   );
+
+
+  payload =
+    new Uint8Array();
+
+
+} else {
+
+  payload =
+    createPlainMessage(messageText);
+
+}
 
   /* トランザクション作成 */
   const descriptor = new appState.sdkSymbol.descriptors.TransferTransactionV1Descriptor(
