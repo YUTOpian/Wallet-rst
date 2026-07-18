51cb1a5681abf061247b94c9a0a36306a1ae45ae
YUTO <63623900+YUTOpian@users.noreply.github.com>
2026-07-18 12:17:22 +0900

    Update message.js

--- js/message.js
+++ js/message.js
@@ -1,5 +1,5 @@
 // message.js
-// TransferTransaction用メッセージ生成
+// Symbol TransferTransaction メッセージ生成
 
 export function createPlainMessage(messageText) {
 
@@ -10,5 +10,49 @@ export function createPlainMessage(messageText) {
     0x00,
     ...msgBytes
   ]);
+
+}
+
+
+/*
+  SSS Extensionによる暗号化メッセージ
+*/
+export async function createEncryptedMessage(
+  messageText,
+  recipientPublicKey
+) {
+
+  const encrypted =
+    await window.SSS.encryptMessage(
+      recipientPublicKey,
+      messageText
+    );
+
+
+  return encrypted;
+
+}
+
+
+/*
+  平文 / 暗号化 切替
+*/
+export async function createMessagePayload(
+  messageText,
+  encrypted,
+  recipientPublicKey
+) {
+
+  if (encrypted) {
+
+    return await createEncryptedMessage(
+      messageText,
+      recipientPublicKey
+    );
+
+  }
+
+
+  return createPlainMessage(messageText);
 
 }
